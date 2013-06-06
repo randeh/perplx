59822da24dd40fa4c1540c9d1130ee86ad92093b
Ryan Jackson <r_j@hotmail.co.uk>
2013-06-06 23:42:07 +0100

    Started client side of a register/login system

--- client.js
+++ client.js
@@ -1,5 +1,56 @@
 "use strict";
 
+window.addEventListener("load", function(event) {
+  var loginPane = document.getElementById("login-pane");
+  var loginUsername = document.getElementById("login-username");
+  var loginPassword = document.getElementById("login-password");
+  var loginButton = document.getElementById("login-button");
+  var loginRegister = document.getElementById("login-register");
+
+  var registerPane = document.getElementById("register-pane");
+  var registerUsername = document.getElementById("register-username");
+  var registerEmail = document.getElementById("register-email");
+  var registerPassword = document.getElementById("register-password");
+  var registerPassword2 = document.getElementById("register-password2");
+  var registerButton = document.getElementById("register-button");
+
+  var homePane = document.getElementById("home-pane");
+  var homeUsername = document.getElementById("home-username");
+
+  var socket = new WebSocket("ws://localhost:8080");
+
+  loginButton.addEventListener("click", function(event) {
+    var username = loginUsername.value;
+    var password = loginPassword.value;
+    loginUsername.value = "";
+    loginPassword.value = "";
+    // send login attempt to server
+    socket.send(username + " " + password);
+  });
+
+  loginRegister.addEventListener("click", function(event) {
+    loginPane.style.visibility = "none";
+    registerPane.style.visibility = "visible";
+    return false;
+  });
+
+  registerButton.addEventListener("click", function(event) {
+    var username = registerUsername.value;
+    var email = registerEmail.value;
+    var password = registerPassword.value;
+    var password2 = registerPassword2.value;
+    // perform validation
+    // clear fields
+    // send register attempt to server
+  });
+
+  socket.addEventListener("message", function(event) {
+    alert(event.data);
+  })
+
+});
+
+/*
 // Initialize everything when the window finishes loading
 window.addEventListener("load", function(event) {
   var status = document.getElementById("status");
@@ -56,3 +107,4 @@ window.addEventListener("load", function(event) {
     text.value = "";
   });
 });
+*/
